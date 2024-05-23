953c3f89db6dabcf8e3daab3311dbf46695aad69
vishag002 <vvishag477@gmail.com>
2024-05-23 19:42:17 +0530

    ui

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,75 @@
+import 'package:firebase_crud/view/crud_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 90,
+        title: Text("Options",
+            style: TextStyle(
+                height: 2,
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.w600)),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CrudScreen(),
+                    ));
+              },
+              child: Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.orange.shade800,
+                    borderRadius: BorderRadius.circular(
+                      30,
+                    )),
+                child: Text(
+                  "CRUD",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Colors.orangeAccent.shade700,
+                  borderRadius: BorderRadius.circular(
+                    25,
+                  )),
+              child: Text(
+                "upload Image",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
