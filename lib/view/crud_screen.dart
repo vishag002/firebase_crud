df68ba8a2bdd256e5a4c842f332329b6179ff2cf
vishag002 <vvishag477@gmail.com>
2024-05-24 16:13:36 +0530

    ui

--- lib/view/crud_screen.dart
+++ lib/view/crud_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CrudScreen extends StatefulWidget {
@@ -20,6 +21,9 @@ class _CrudScreenState extends State<CrudScreen> {
 
   //delete function
 
+  //fire base
+  CollectionReference employeeCollection =
+      FirebaseFirestore.instance.collection('employee');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,81 +76,127 @@ class _CrudScreenState extends State<CrudScreen> {
                 thickness: 3,
               ),
               Expanded(
-                child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (context, index) => ListTile(
-                      title: Text("name"),
-                      subtitle: Text("age"),
-                      trailing: Container(
-                        width: 100,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                                onPressed: () => showModalBottomSheet(
-                                      enableDrag: true,
-                                      showDragHandle: true,
-                                      isDismissible: true,
-                                      context: context,
-                                      builder: (context) => Container(
-                                        height: 300,
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(30.0),
-                                          child: Column(
-                                            children: [
-                                              TextField(
-                                                controller: nameController,
-                                                decoration: InputDecoration(
-                                                  alignLabelWithHint: true,
-                                                  hintText: "name",
-                                                  border:
-                                                      UnderlineInputBorder(),
-                                                ),
-                                                cursorHeight: 20,
-                                                keyboardType:
-                                                    TextInputType.number,
-                                              ),
-                                              SizedBox(height: 20),
-                                              TextField(
-                                                controller: ageController,
-                                                decoration: InputDecoration(
-                                                  alignLabelWithHint: true,
-                                                  hintText: "age",
-                                                  border:
-                                                      UnderlineInputBorder(),
-                                                ),
-                                                cursorHeight: 20,
-                                                keyboardType:
-                                                    TextInputType.number,
-                                              ),
-                                              SizedBox(height: 40),
-                                              ElevatedButton(
-                                                onPressed: () {},
-                                                child: Text(
-                                                  "Confirm",
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 18,
-                                                    fontWeight: FontWeight.w900,
+                /* child: StreamBuilder(
+                  stream: employeeCollection.snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            //Document snap shot
+                            DocumentSnapshot employee =
+                                snapshot.data!.docs[index];
+                            return ListTile(
+                              title: Text("${employee['name']}"),
+                              subtitle: Text("${employee['age']}"),
+                            );
+                          });
+                    } else {
+                      return Container(child: Text("No Data Found"));
+                    }
+                  },
+                ), */
+                child: StreamBuilder(
+                  stream: employeeCollection.snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            DocumentSnapshot employee =
+                                snapshot.data!.docs[index];
+                            return ListTile(
+                                title: Text("${employee['name']}"),
+                                subtitle: Text("${employee['age']}"),
+                                trailing: Container(
+                                  width: 100,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      IconButton(
+                                          onPressed: () => showModalBottomSheet(
+                                                enableDrag: true,
+                                                showDragHandle: true,
+                                                isDismissible: true,
+                                                context: context,
+                                                builder: (context) => Container(
+                                                  height: 300,
+                                                  child: Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            30.0),
+                                                    child: Column(
+                                                      children: [
+                                                        TextField(
+                                                          controller:
+                                                              nameController,
+                                                          decoration:
+                                                              InputDecoration(
+                                                            alignLabelWithHint:
+                                                                true,
+                                                            hintText: "name",
+                                                            border:
+                                                                UnderlineInputBorder(),
+                                                          ),
+                                                          cursorHeight: 20,
+                                                          keyboardType:
+                                                              TextInputType
+                                                                  .name,
+                                                        ),
+                                                        SizedBox(height: 20),
+                                                        TextField(
+                                                          controller:
+                                                              ageController,
+                                                          decoration:
+                                                              InputDecoration(
+                                                            alignLabelWithHint:
+                                                                true,
+                                                            hintText: "age",
+                                                            border:
+                                                                UnderlineInputBorder(),
+                                                          ),
+                                                          cursorHeight: 20,
+                                                          keyboardType:
+                                                              TextInputType
+                                                                  .number,
+                                                        ),
+                                                        SizedBox(height: 40),
+                                                        ElevatedButton(
+                                                          onPressed: () {},
+                                                          child: Text(
+                                                            "Confirm",
+                                                            style: TextStyle(
+                                                              color:
+                                                                  Colors.white,
+                                                              fontSize: 18,
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w900,
+                                                            ),
+                                                          ),
+                                                          style: ButtonStyle(
+                                                              backgroundColor:
+                                                                  MaterialStatePropertyAll(
+                                                                      Colors
+                                                                          .orangeAccent)),
+                                                        ),
+                                                      ],
+                                                    ),
                                                   ),
                                                 ),
-                                                style: ButtonStyle(
-                                                    backgroundColor:
-                                                        MaterialStatePropertyAll(
-                                                            Colors
-                                                                .orangeAccent)),
                                               ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                icon: Icon(Icons.edit)),
-                            IconButton(
-                                onPressed: () {}, icon: Icon(Icons.delete))
-                          ],
-                        ),
-                      )),
+                                          icon: Icon(Icons.edit)),
+                                      IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(Icons.delete))
+                                    ],
+                                  ),
+                                ));
+                          });
+                    } else {
+                      return Container(child: Text("No Data Found"));
+                    }
+                  },
                 ),
               ),
             ],
