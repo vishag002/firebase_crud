5a27107388c11118059447dcd3ffc99e90a1d401
vishag002 <vvishag477@gmail.com>
2024-05-24 16:12:56 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,18 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crud/view/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'view/crud_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: 'AIzaSyD1ZAofC1XH5feJvwq8_7c_YQotKEtceog',
-          appId: '1:494769175859:android:0e38fccc2a6d8636bed246',
-          messagingSenderId: '',
-          projectId: 'fir-demo-678f4',
-          storageBucket: 'fir-demo-678f4.appspot.com'));
-  runApp(const MyApp());
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: CrudScreen(),
     );
   }
 }
